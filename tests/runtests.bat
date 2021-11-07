361ba9203473f16f4d60443693cdd6b1380af884
LMurphy001 <gh@linda-murphy.net>
2021-11-07 16:46:46 -0500

    Added sqlite testing

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -1,4 +1,12 @@
 @echo off
-rem cls
-php testjson.php data\stores_data.json ..\resources\html_doc
+cls
+
+rem php testcsv.php  data\csv\store_list.csv ..\resources\html_doc
+rem pause
+
+php testjson.php   data\stores_data.json   ..\resources\md_doc
+php testsqlite.php data\stores.sqlite      ..\resources\md_doc
+
+rem pause
+rem pause
 rem php testjson.php data\stores_data.json ..\resources\md_doc 
---
+++ tests/testsqlite.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+$curDir = __DIR__ .DIRECTORY_SEPARATOR. '.' .DIRECTORY_SEPARATOR;
+require_once $curDir . 'Main.php';
+require_once $curDir . 'LMFoundry_autoload.php';
+use LM\Foundry\{Cast, Utils, Results};
+
+function runSqlite(string $dbFilename, string $moldFilename) : Results {
+    // ====>>>> TO DO:
+    // Open the sqlite db in the file
+    // Do some queries. 
+    // Make the queries ready for the 'pour' function
+    // call cast->pour() method
+    $results = new Results;
+    $pdo = new PDO('sqlite:data\\stores.sqlite');
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $sql = 'SELECT store_name, rating FROM store';
+    $sth = $pdo->prepare($sql);
+    $sth->execute();
+    $stores = $sth->fetchAll();
+    $stores_data = [];
+
+    foreach ($stores as $store) {
+        $record = [
+            'store_name'=>$store['store_name'],
+            'store_rating'=>$store['rating'],
+            'fruits' => []
+        ];
+        $sql = 'SELECT name, color, price FROM fruit where store_name = ?';
+        $sth = $pdo->prepare($sql);
+        $sth->bindValue(1, $store['store_name'], PDO::PARAM_STR);
+        $sth->execute();
+        $fruits = $sth->fetchAll();
+        foreach ($fruits as $fruit) {
+            $record['fruits'][] = [
+                'fruit_name' => $fruit['name'],
+                'color' => $fruit['color'], 
+                'price' => $fruit['price']
+            ];
+        }
+        $stores_data[] = $record;
+    }
+    $data = [
+        "doc_title" => "Fruit Prices at Nearby Stores",
+        "store_list" => $stores_data
+    ];
+
+    $results = Cast::pour($moldFilename, $data);
+    return $results;
+}
+
+echo Main::main($argc, $argv, 'sqlite', 'runSqlite');
